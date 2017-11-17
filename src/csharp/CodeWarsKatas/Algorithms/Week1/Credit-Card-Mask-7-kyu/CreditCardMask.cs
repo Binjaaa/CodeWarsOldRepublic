e4ffd0803b5054bbf4881895334d69afef4e21cb
Imre Turi <Imre.Turi@kpmg.co.uk>
2017-11-17 10:30:53 +0000

    Add CreditCardMask algo

---
+++ src/csharp/CodeWarsKatas/Algorithms/Week1/Credit-Card-Mask-7-kyu/CreditCardMask.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWarsKatas.Algorithms.Week1.Credit_Card_Mask_7_kyu
+{
+    internal class CreditCardMask
+    {
+        public static string Maskify(string str)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+                return string.Empty;
+
+            var length = str.Length;
+
+            if (length > 4)
+            {
+                var visibleStartIndex = length - 4;
+                var secretPart = new string('#', visibleStartIndex);
+                var visiblePart = str.Substring(visibleStartIndex);
+                return $"{secretPart}{visiblePart}";
+            }
+
+            return str;
+        }
+    }
+}
