d07b3a7adbf9130a42a87da093abf42ee39f20d1
Imre Turi <Imre.Turi@kpmg.co.uk>
2017-11-17 10:32:04 +0000

    Add "Square Every Digit" algo.

---
+++ src/csharp/CodeWarsKatas/Algorithms/Week1/Square-Every-Digit-7-kyu/SquareEveryDigitImplementation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWarsKatas.Algorithms.Week1.Square_Every_Digit_7_kyu
+{
+    internal class SquareEveryDigitImplementation
+    {
+        public static int SquareEveryDigit(int n)
+        {
+            var newNumber = n
+                .ToString()
+                .Select(c => Math.Pow(char.GetNumericValue(c), 2));
+
+            return Convert.ToInt32(string.Join("", newNumber));
+        }
+    }
+}
