56132fd2ff942ae026ccab4a61a61fb04db0cfa5
Imre Turi <Imre.Turi@kpmg.co.uk>
2017-11-17 10:32:20 +0000

    Add "Sum of Odd numbers" algo.

---
+++ src/csharp/CodeWarsKatas/Algorithms/Week1/Sum-of-odd-numbers-7-kyu/SumOfOddNumbersImpl.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CodeWarsKatas.Algorithms.Week1.Sum_of_odd_numbers_7_kyu
+{
+    internal class SumOfOddNumbersImpl
+    {
+        public static int Solve(string s)
+        {
+            return new Regex(@"\d+")
+                .Matches(s)
+                .Cast<Match>()
+                .OrderByDescending(x => x.Length)
+                .Select(x => int.Parse(x.Value))
+                .Max();
+        } 
+    }
+}
