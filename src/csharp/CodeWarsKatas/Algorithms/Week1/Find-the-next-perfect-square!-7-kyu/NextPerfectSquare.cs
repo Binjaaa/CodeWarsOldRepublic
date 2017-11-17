9aaf4f4b913e86a119837dc11b54654bce3aed97
Imre Turi <Imre.Turi@kpmg.co.uk>
2017-11-17 10:31:42 +0000

    Add "Next Perfect Square" algo

---
+++ src/csharp/CodeWarsKatas/Algorithms/Week1/Find-the-next-perfect-square!-7-kyu/NextPerfectSquare.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWarsKatas.Algorithms.Week1.Find_the_next_perfect_square__7_kyu
+{
+    internal class NextPerfectSquare
+    {
+        public static long NextPerfectSqrt(long num)
+        {
+            return new[] { (long)Math.Sqrt(num) }
+                .Select(sqrt =>
+                {
+                    var perfectSqrt = sqrt * sqrt;
+                    var nextSqrtBase = sqrt + 1;
+
+                    return num == perfectSqrt ? nextSqrtBase * nextSqrtBase : -1;
+                })
+                .First();
+        }
+    }
+}
