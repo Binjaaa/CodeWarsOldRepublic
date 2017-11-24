061e0f4529771bcd4b7404324d75a410467d3434
Jozsef Turi <josephturi@gmail.com>
2017-11-24 12:13:46 +0000

    [WIP] Add "Max and min numbers"

---
+++ src/python/PyWeek1/max_and_min_numbers.py
@@ -0,0 +1,20 @@
+def high_and_low(numbers):
+    list1 = []
+    numbers = numbers.replace(" ", ", ")
+    list1.append(max(numbers))
+    return numbers
+
+
+
+
+
+
+# ----------------------------------------------
+print high_and_low("4 5 29 54 4 0 -214 542 -64 1 -3 6 -6") #  "542 -214");
+print high_and_low("1 -1") #  "1 -1");
+print high_and_low("1 1") #  "1 1");
+print high_and_low("-1 -1") #  "-1 -1");
+print high_and_low("1 -1 0") #  "1 -1");
+print high_and_low("1 1 0") #  "1 0");
+print high_and_low("-1 -1 0") #  "0 -1");
+print high_and_low("42") #  "42 42");
