90d70ea625024ae1e66a0ca5a2fe0954e6d9d0c0
Jozsef Turi <josephturi@gmail.com>
2017-11-24 13:57:59 +0000

    Add Max and min numbers

--- src/python/PyWeek1/max_and_min_numbers.py
+++ src/python/PyWeek1/max_and_min_numbers.py
@@ -1,8 +1,7 @@
 def high_and_low(numbers):
-    list1 = []
-    numbers = numbers.replace(" ", ", ")
-    list1.append(max(numbers))
-    return numbers
+    numbers1 = [int(n) for n in numbers.split()]
+    list1 = str(max(numbers1)) + " " + str(min(numbers1))
+    return list1
 
 
 
@@ -10,11 +9,11 @@ def high_and_low(numbers):
 
 
 # ----------------------------------------------
-print high_and_low("4 5 29 54 4 0 -214 542 -64 1 -3 6 -6") #  "542 -214");
-print high_and_low("1 -1") #  "1 -1");
-print high_and_low("1 1") #  "1 1");
-print high_and_low("-1 -1") #  "-1 -1");
-print high_and_low("1 -1 0") #  "1 -1");
-print high_and_low("1 1 0") #  "1 0");
-print high_and_low("-1 -1 0") #  "0 -1");
-print high_and_low("42") #  "42 42");
+# print high_and_low("4 5 29 54 4 0 -214 542 -64 1 -3 6 -6") #  "542 -214");
+# print high_and_low("1 -1") #  "1 -1");
+# print high_and_low("1 1") #  "1 1");
+# print high_and_low("-1 -1") #  "-1 -1");
+# print high_and_low("1 -1 0") #  "1 -1");
+# print high_and_low("1 1 0") #  "1 0");
+# print high_and_low("-1 -1 0") #  "0 -1");
+# print high_and_low("42") #  "42 42");
