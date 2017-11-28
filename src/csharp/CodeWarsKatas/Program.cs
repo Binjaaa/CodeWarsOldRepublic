13b4db5aba8c090deee0f81e804c8cd1b7479b8d
Imre Turi <Imre.Turi@kpmg.co.uk>
2017-11-28 12:55:43 +0000

    Clean Main.

--- src/csharp/CodeWarsKatas/Program.cs
+++ src/csharp/CodeWarsKatas/Program.cs
@@ -11,9 +11,6 @@ namespace CodeWarsKatas
     {
         static void Main(string[] args)
         {
-            //var res = DeleteNthOccurence.DeleteNth(new int[] { 20, 37, 20, 21 }, 1);
-            var ressss = DeleteNthOccurence.DeleteNth(new int[] { 1, 1, 3, 3, 7, 2, 2, 2, 2 }, 3);
-            ;
         }
     }
 }
