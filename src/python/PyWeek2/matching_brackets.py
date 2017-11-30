258a8dae55be0a816884644a27639fabc5245ee3
Jozsef Turi <josephturi@gmail.com>
2017-11-30 14:13:09 +0000

    [REF] matching_brackets

--- src/python/PyWeek2/matching_brackets.py
+++ src/python/PyWeek2/matching_brackets.py
@@ -7,8 +7,7 @@ def group_check(s):
         if s[i] in brackets:
             list1.append(s[i])
     string_brackets = ''.join(list1)
-    length_string_brackets = len(string_brackets)
-    while length_string_brackets > 0 and ('{}' in string_brackets or '[]' in string_brackets or '()' in string_brackets):
+    while len(string_brackets) > 0 and ('{}' in string_brackets or '[]' in string_brackets or '()' in string_brackets):
         current_pair = pairs[j]
         if current_pair in string_brackets and j < 3:
             string_brackets = string_brackets.replace(current_pair,'')
@@ -17,13 +16,7 @@ def group_check(s):
         else:
             j += 1
 
-    if len(string_brackets) > 0:
-        print  False
-        return False
-    else:
-        print True
-        return True
-
+    return len(string_brackets) <= 0
 
 # group_check("")
-group_check("{([{(123)[456]}])(}")
+print(group_check("{([{(123)[456]}])}[]"))
