696984872c35422bf73b6a7a9c8e992bb23def01
imre.turi <binya87@gmail.com>
2017-12-03 18:31:06 +0000

    Csharp build script - Appveyor  (#9)

---
+++ src/csharp/build.fsx
@@ -0,0 +1,17 @@
+﻿// include Fake lib
+#r "packages/FAKE/tools/FakeLib.dll"
+open Fake
+
+// Properties
+
+Target "BuildApp" (fun _ ->
+    !! "CodeWarsKatas.sln"
+    |> MSBuildRelease null "Build"
+    |> ignore
+)
+
+// Dependencies
+"BuildApp"
+
+// start build
+RunTargetOrDefault "BuildApp"
