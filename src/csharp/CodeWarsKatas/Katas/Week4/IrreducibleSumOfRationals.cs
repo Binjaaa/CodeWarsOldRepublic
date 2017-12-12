471a9cd52731d2d978fa43cdcbc2e38e4b5ea4aa
Imre Turi <Imre.Turi@kpmg.co.uk>
2017-12-12 14:45:30 +0000

    Changes. Merge with commit.

--- src/csharp/CodeWarsKatas/Katas/Week4/IrreducibleSumOfRationals.cs
+++ src/csharp/CodeWarsKatas/Katas/Week4/IrreducibleSumOfRationals.cs
@@ -23,9 +23,7 @@ namespace CodeWarsKatas.Katas.Week4
         {
             var gcd = GreatestCommonDivisor(rational.N, rational.D);
 
-            var gg = new Rational(rational.N / gcd, rational.D / gcd);
-
-            return gg;
+            return new Rational(rational.N / gcd, rational.D / gcd);
         }
 
         private static int LeastCommonMultiple(int d1, int d2)
@@ -42,14 +40,14 @@ namespace CodeWarsKatas.Katas.Week4
 
         private static IEnumerable<Rational> SetLeastCommonMultiple(List<Rational> rationals)
         {
-            Func<Rational, int, Rational> SetCommonDenominator = (r, lcm) =>
+            Func<Rational, int, Rational> setCommonDenominator = (r, lcm) =>
                 r.D != lcm
                     ? new Rational(lcm / r.D * r.N, lcm)
                     : r;
 
             var leastCommonMultiple = LeastCommonMultiple(rationals);
 
-            return rationals.Select(r => SetCommonDenominator(r, leastCommonMultiple));
+            return rationals.Select(r => setCommonDenominator(r, leastCommonMultiple));
         }
 
         public static string SumFracts(int[,] l)
@@ -57,16 +55,13 @@ namespace CodeWarsKatas.Katas.Week4
             if (l == null || l.Length == 0)
                 return null;
 
-            var ff = LogHelper.Log(l);
-            LogHelper.LogConsole(ff);
-
             var fractions = Enumerable.Range(0, l.GetLength(0))
                 .Select(index => SimplifyFraction(new Rational(l[index, 0], l[index, 1])));
 
             var res = SetLeastCommonMultiple(fractions.ToList())
                 .Aggregate((r1, r2) => new Rational(r1.N + r2.N, r2.D));
 
-            return res.ToString();
+            return SimplifyFraction(res).ToString();
         }
 
         internal struct Rational
