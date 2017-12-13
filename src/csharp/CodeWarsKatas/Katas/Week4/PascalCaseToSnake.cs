36cfb3688ffa6e326369306c6896263c8ca8eecd
Imre Turi <Imre.Turi@kpmg.co.uk>
2017-12-13 14:34:23 +0000

    Add Pascal to snake file name converter.

---
+++ src/csharp/CodeWarsKatas/Katas/Week4/PascalCaseToSnake.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace CodeWarsKatas.Katas.Week4
+{
+    internal class PascalCaseToSnake
+    {
+        public static string ToUnderscore(int str)
+        {
+            return str.ToString();
+        }
+
+        public static string ToUnderscore(string str)
+        {
+            var query = str.Select((c, i) =>
+            {
+                if (i == 0)
+                    return char.ToLowerInvariant(c).ToString();
+
+                return char.IsUpper(c)
+                    ? $"_{char.ToLowerInvariant(c)}"
+                    : c.ToString();
+            });
+
+            return string.Join("", query);
+        }
+
+        public static string ToUnderscore2(string str)
+        {
+            var subPart = Regex
+                .Replace(str, @"\B[A-Z]", m => $"_{m.ToString().ToLower()}")
+                .Substring(1);
+
+            return char.ToLowerInvariant(str[0]) + subPart;
+        }
+    }
+}
