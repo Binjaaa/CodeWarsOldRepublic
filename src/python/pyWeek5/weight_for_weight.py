e58b134aec9fbe374359e196a5b07551348de05e
jturi <josephturi@gmail.com>
2019-10-08 09:08:54 +0100

    Delete files that are moved

--- src/python/pyWeek5/weight_for_weight.py
+++
@@ -1,8 +0,0 @@
-def order_weight(strng):
-    num_list = [(str(x)) for x in list(strng.split(" "))]
-    dict_list = [{sum(map(int, str(x))):[x]} for x in num_list if len(x) > 0]
-    result = ""
-    for i in sorted(dict_list):
-        for key, value in i.items():
-            result += value[0] + " "
-    return result[:-1]
--- src/python_solutions/equal_sides_of_an_array.py
+++ src/python_solutions/equal_sides_of_an_array.py
@@ -10,6 +10,7 @@ def find_even_index(arr):
 
 
 if __name__ == '__main__':
+    log.debug("Tests started.")
     Test.assert_equals(find_even_index([1, 2, 3, 4, 3, 2, 1]), 3)
     Test.assert_equals(find_even_index([1, 100, 50, -51, 1, 1]), 1)
     Test.assert_equals(find_even_index([1, 2, 3, 4, 5, 6]), -1)
